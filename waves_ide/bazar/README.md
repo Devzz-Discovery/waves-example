0f90f3af4e235d5a7dccb2ec31dc70c5f81dc386
devzz-space <contact@devzz.space>
2022-02-06 18:58:14 +0300

    пример контракта и его вызовы через js-тесты

---
+++ waves_ide/bazar/README.md
@@ -0,0 +1,4 @@
+# waves-example
+Пример работы с Waves IDE на https://waves-ide.com/
+
+dapp-приложение по продаже скидочных купонов.
---
+++ waves_ide/bazar/bazar.js
@@ -0,0 +1,61 @@
+let datajson = {
+    "title": "t-Shit",
+    "coupon_price": 100,
+    "old_price": 1500,
+    "new_price": 1100,
+    "address": "Universe",
+    "description": "t-Shirt coupon",
+    "image": "www.google.com"
+}
+
+const WAVES = 10 ** 8;
+
+const BAZAR_WAVES = 0.05 * WAVES
+const SUPPLIER_WAVES = 0.05 * WAVES
+const CUSTOMER_WAVES = 0.05 * WAVES
+
+describe('Coupon bazar test suite', async function () {
+
+    before(async function () {
+
+        await setupAccounts({bazar: BAZAR_WAVES, supplier: SUPPLIER_WAVES, customer: CUSTOMER_WAVES});
+        
+        const scriptC = compile(file('addItem.ride'));
+        const ssTx = setScript({script:scriptC}, accounts.bazar);
+        await broadcast(ssTx);
+        await waitForTx(ssTx.id)
+        console.log('Script has been set')
+    });    
+    
+    it('add new bazar item from supplier', async function(){
+        let ts = invokeScript({
+            dApp: address(accounts.bazar),
+            call:{
+                function:"addItem",
+                args:[
+                    {type:"string", value: datajson.title},
+                    {type: "integer", value: datajson.coupon_price},
+                    {type: "string", value: JSON.stringify(datajson)}        
+                ]},
+                payment: []
+        }, accounts.supplier)
+        let tx = await broadcast(ts)
+        await waitForTx(tx.id)
+    })
+
+    it('purchase item', async function() {
+        let item = "item_" + accounts.customer
+         console.log("Cur height: " + item)
+        let ts = invokeScript({
+            dApp: address(accounts.bazar),
+            call: {
+                function: "purchase",
+                args:[
+                    {type: "string", value: item}
+                ]},
+                payment: [{amount: datajson.coupon_price, assetId: null}]
+            }, accounts.customer)
+        let tx = await broadcast(ts)
+        await waitForTx(tx.id)
+    })
+})
